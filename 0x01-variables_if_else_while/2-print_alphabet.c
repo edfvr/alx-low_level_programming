ff8be428c100ec1de7a1a1a237487be555420300
Edmund Sagoe <edsagoefvr@gmail.com>
2022-06-03 19:42:03 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - prints the alphabet in lowercase, followed by a new line.
+ *Return: 0 to exit successfully
+ */
+
+int main(void)
+{
+	int i;
+	for(i = 97; i <= 122; i++)
+	{
+		putchar{i};
+	}
+	putchar{10};
+	return 0;
+}
