2be9a95ac9b8205e48f2da81ab8fd2084df5560d
Edmund Sagoe <edsagoefvr@gmail.com>
2022-06-06 19:31:46 -0700

    Variables, if, else, while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,12 @@
 
 int main(void)
 {
-       	int i;
-       	for (i = 97; i <= 122; i++)
+	int i;
+	
+	for (i = 97; i <= 122; i++)
        	{
 		putchar(i);
 	}
-	putchar(10);
+	putchar (10);
 	return (0);
 }
