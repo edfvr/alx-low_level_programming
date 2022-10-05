832a835c38d81db68bf91a670e2b55032b71c765
Ed <edsagoefvr@gmail.com>
2022-10-05 23:57:02 +0000

    So tired

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc : number of arguments passed to the program
+ * @argv : array of strings
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	if (argc > 0)
+	{
+		printf("%d\n", argc - 1);
+	}
+	return (0);
+}
