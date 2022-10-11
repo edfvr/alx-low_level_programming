af3a861cd1e499408e73ca9f1292a96fdf97b0ec
Ed <edsagoefvr@gmail.com>
2022-10-11 21:45:20 +0000

    Mr. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Some dog's description
+ * @name : Dog's name
+ * @age : Dog's age
+ * @owner : Dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG_H*/
