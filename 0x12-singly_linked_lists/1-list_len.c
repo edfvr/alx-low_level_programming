20b16bc69c4ab9a6e73e27b5de90150c274a97bd
Edmund Sagoe <edsagoefvr@gmail.com>
2022-10-20 13:54:58 -0700

    betty

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,9 +4,10 @@
  * list_len - returns the number of elements in a linked list_t list.
  * @h : linked list
  */
- size_t list_len(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t i;
+
 	i = 0;
 
 	while (h != 0)
