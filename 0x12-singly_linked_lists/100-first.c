ac1536663489b78234d93ce2298c4b53026279de
Edmund Sagoe <edsagoefvr@gmail.com>
2022-10-20 17:46:41 -0700

    hare & tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+/**
+ * _first - prints a string before the main function is executed
+ */
+void _first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
