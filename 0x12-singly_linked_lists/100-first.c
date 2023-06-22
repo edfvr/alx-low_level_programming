c6862586bf0aef6a687282d3e847b13cb9efb453
Edmund Sagoe <edsagoefvr@gmail.com>
2023-06-22 16:30:34 +0000

    Delete 0x12-singly_linked_lists directory

--- 0x12-singly_linked_lists/100-first.c
+++
@@ -1,9 +0,0 @@
-#include "lists.h"
-/**
- * _first - prints a string before the main function is executed
- */
-void _first(void)
-{
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
-}
