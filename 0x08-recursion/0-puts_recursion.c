00501ab772db9393e396a392c4233cee4e282480
Ed <edsagoefvr@gmail.com>
2023-10-25 06:02:31 -0700

    [edit]: betty style

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,7 +5,8 @@
  *_puts_recursion - prints a string followed by a new line
  * @s: string
  */
-void _puts_recursion(char *s) {
+void _puts_recursion(char *s)
+{
 	if (*s == '\0')
 	{
 		_putchar('\n');
