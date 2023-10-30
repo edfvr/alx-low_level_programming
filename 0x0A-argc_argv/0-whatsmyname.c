1affd5cab4e6e2a745a3bd10e8de3d5eb46e5f47
Ed <edsagoefvr@gmail.com>
2023-10-30 16:18:21 -0700

    check task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - prints name of program, then new line
+ * @argc : number of arguments passed to the program
+ * @argv : array of strings
+ * Return: 0
+ */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
